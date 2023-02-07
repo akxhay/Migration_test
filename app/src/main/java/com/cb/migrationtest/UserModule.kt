99a076956bc1670aee08edbb8d34f1da59186e36
Akshay Sharma <akshay.sharma@africa.airtel.com>
2023-02-07 15:04:27 +0530

    migration working

--- app/src/main/java/com/cb/migrationtest/UserModule.kt
+++ app/src/main/java/com/cb/migrationtest/UserModule.kt
@@ -2,8 +2,8 @@ package com.cb.migrationtest
 
 import android.content.Context
 import androidx.room.Room
-import com.cb.cb_test.application.data.database.UserDatabase
-import com.cb.cb_test.application.data.database.UserDatabase.Companion.DATABASE_NAME
+import com.cb.migrationtest.UserDatabase.Companion.DATABASE_NAME
+import com.cb.migrationtest.UserRoomDatabase.Companion.MIGRATION_1_2
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,13 +17,14 @@ class SenderModule {
 
 
     @Provides
-    fun providesDao(userDatabase: UserDatabase): UserDao =
+    fun providesDao(userDatabase: UserRoomDatabase): UserDao =
         userDatabase.userDao()
 
     @Provides
     @Singleton
-    fun providesDatabase(@ApplicationContext context: Context): UserDatabase =
-        Room.databaseBuilder(context, UserDatabase::class.java, DATABASE_NAME)
+    fun providesDatabase(@ApplicationContext context: Context): UserRoomDatabase =
+        Room.databaseBuilder(context, UserRoomDatabase::class.java, DATABASE_NAME)
+            .addMigrations(MIGRATION_1_2)
             .build()
 
 
--- app/src/main/java/com/cb/migrationtest/UserRoomDatabase.kt
+++ app/src/main/java/com/cb/migrationtest/UserRoomDatabase.kt
@@ -1,18 +1,39 @@
-package com.cb.cb_test.application.data.database
+package com.cb.migrationtest
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.cb.migrationtest.UserDao
-import com.cb.migrationtest.UserRecord
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
+
 
 @Database(
     entities = [UserRecord::class],
-    version = 1
+    version = 2
 )
-abstract class UserDatabase : RoomDatabase() {
+abstract class UserRoomDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 
     companion object {
-        const val DATABASE_NAME = "user_database"
+
+        const val TABLE_NAME_USER = "user_record"
+        const val COLUMN_ID = "id"
+        const val COLUMN_USER_NAME = "user_name"
+
+        private var createUserTable = ("create table if not exists " + TABLE_NAME_USER
+                + " ( " + COLUMN_ID + " INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT," + COLUMN_USER_NAME + " TEXT NOT NULL UNIQUE);")
+
+        private val indices =
+            "CREATE UNIQUE INDEX index_user_record_user_name ON user_record(user_name); "
+        private const val updateUserName =
+            "insert into user_record (id, user_name)   SELECT id,  (FIRST_NAME || ' ' ||   LAST_NAME ) as name from USER ;"
+
+        val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL(createUserTable)
+                database.execSQL(indices)
+                database.execSQL(updateUserName)
+            }
+        }
     }
 }
+
